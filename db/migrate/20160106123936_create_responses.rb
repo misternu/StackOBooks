fc3b23a0217ea6ecaec28cf6faf666a871181d71
Brian Wagner & Timothy \"Manu\" Meixell <pair+hazelmaeby.misternu@devbootcamp.com>
2016-01-06 14:15:18 -0600

    Clean up seeds files, fix table for responses

--- db/migrate/20160106123936_create_responses.rb
+++ db/migrate/20160106123936_create_responses.rb
@@ -1,11 +1,12 @@
 class CreateResponses < ActiveRecord::Migration
   def change
-    create_table :users do |t|
+    create_table :responses do |t|
       t.string :body
-      t.reference :user
+      t.references :user
       t.integer :comment_id
       t.string :comment_type
 
       t.timestamps
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,26 @@
+require 'faker'
+
 50.times do
-  User.new( username: Faker::Internet.user_name,
+  User.create( username: Faker::Internet.user_name,
             hashed_password: "password")
 end
 
 20.times do
-  Post.new( user_id: rand(1..50),
-           title: Faker::Book.title + Faker::Lorem.words(4),
-           body: Faker::Lorem.sentences(4) + Faker::Hipster.sentence )
+  Post.create( user_id: rand(1..50),
+           title: Faker::Lorem.words(4),
+           body: Faker::Lorem.sentences(4) )
 end
 
 20.times do
-  Response.new( user_id: rand(1..50),
-                body: Faker::Lorem.sentences(2) + Faker::Hipster.sentence,
+  Response.create( user_id: rand(1..50),
+                body: Faker::Lorem.sentences(2),
                 comment_id: rand(1..20),
                 comment_type: "Post" )
 end
 
 30.times do
-    Response.new( user_id: rand(1..50),
-                body: Faker::Lorem.sentences(2) + Faker::Hipster.sentence,
+    Response.create( user_id: rand(1..50),
+                body: Faker::Lorem.sentences(2),
                 comment_id: rand(1..20),
                 comment_type: "Response" )
 end
