8af29c35b7fe7827aee62aeaa6f96d67978d9d8b
Brian Wagner & Timothy \"Manu\" Meixell <pair+hazelmaeby.misternu@devbootcamp.com>
2016-01-06 17:56:41 -0600

    Make posts pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,20 @@ end
 
 20.times do
   Post.create( user_id: rand(1..50),
-           title: Faker::Lorem.words(4),
-           body: Faker::Lorem.sentences(4) )
+           title: Faker::Lorem.words(4).join(" "),
+           body: Faker::Lorem.sentences(4).join(" ") )
 end
 
 20.times do
   Response.create( user_id: rand(1..50),
-                body: Faker::Lorem.sentences(2),
+                body: Faker::Lorem.sentences(2).join(" "),
                 comment_id: rand(1..20),
                 comment_type: "Post" )
 end
 
 30.times do
     Response.create( user_id: rand(1..50),
-                body: Faker::Lorem.sentences(2),
+                body: Faker::Lorem.sentences(2).join(" "),
                 comment_id: rand(1..20),
                 comment_type: "Response" )
 end
