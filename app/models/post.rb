84c74086ec26d98d016426ecbe0c88d703280c65
Brian Wagner & Timothy \"Manu\" Meixell <pair+hazelmaeby.misternu@devbootcamp.com>
2016-01-06 16:42:06 -0600

    Add favorites table and model to catch fave response for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :taggings
   has_many :tags, through: :taggings
+
+  has_one :favorite
+  has_one :accepted, through: :favorite, source: :response
+
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -3,4 +3,7 @@ class Response < ActiveRecord::Base
   belongs_to :comment, polymorphic: true
   has_many :responses, as: :comment
   has_many :votes
+
+  has_one :favorite
+  has_one :post, through: :favorite
 end
