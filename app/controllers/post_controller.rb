d2aa46c3a558e24ec4b256e6c60f60e99b1241a2
Lars Johnson & Chris Gomes <pair+larsjx.ugaliguy@devbootcamp.com>
2016-01-06 19:44:06 -0600

    Add simple post creation form

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -3,6 +3,17 @@ get '/posts' do
   erb :"posts"
 end
 
+get '/posts/new' do
+  erb :"posts/new"
+end
+
+post '/posts' do
+  @posts = Post.all
+  Post.create(params[:post])
+  erb :"posts/index"
+end
+
+
 get '/posts/:id' do
   @post = Post.find(params[:id])
   @responses = @post.responses
