af0ac2c3b29834ac011ed82aac1d8a8a4910b8cb
Lars Johnson & Timothy \"Manu\" Meixell <pair+larsjx.misternu@devbootcamp.com>
2016-01-07 11:34:35 -0600

    Make form for adding responses to post

---
+++ app/helpers/add_response.rb
@@ -0,0 +1,6 @@
+helpers do
+  def add_response(respondable, response_body)
+
+    #post_id
+  end
+end
--- app/models/response.rb
+++ app/models/response.rb
@@ -3,7 +3,14 @@ class Response < ActiveRecord::Base
   belongs_to :comment, polymorphic: true
   has_many :responses, as: :comment
   has_many :votes
-
   has_one :favorite
   has_one :post, through: :favorite
+
+  validates :body, presence: true
+  validates_length_of :body, minimum: 2, too_short: 'Please enter at least 2 characters.'
+
+  def post_id
+    return self.comment.id if self.comment.is_a?(Post)
+    self.comment.post_id #recursion!
+  end
 end
