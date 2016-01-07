c86613899b05d4eca0bde34a8de6447ce3521b5d
Lars Johnson & Chris Gomes <pair+larsjx.ugaliguy@devbootcamp.com>
2016-01-07 17:38:28 -0600

    Correct order of /posts/new and /posts/:id

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -3,20 +3,23 @@ get '/posts' do
   erb :"posts/index"
 end
 
+get '/posts/new' do
+  erb :"posts/new"
+end
+
 get '/posts/:id' do
   @post = Post.find(params[:id])
   @responses = @post.responses
   erb :"post_detail"
 end
 
-get '/posts/new' do
-  erb :"posts/new"
-end
-
 post '/posts' do
-  @posts = Post.all
-  Post.create(params[:post])
-  erb :"posts/index"
+  @post = Post.new(params[:post].merge(user_id: session[:user_id]))
+  if @post.save
+    redirect '/posts'
+  else
+    "error"
+  end
 end
 
 
